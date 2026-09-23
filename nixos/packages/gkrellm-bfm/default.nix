e08a9a5f91dc61b882a2c1d2f0cd78b9d77e6268
Meg Richards <merichar@merichar.org>
2026-09-22 21:17:24 -0400

    fix(nixos): connect BFM spin callback directly

--- nixos/packages/gkrellm-bfm/default.nix
+++ nixos/packages/gkrellm-bfm/default.nix
@@ -25,11 +25,27 @@ stdenv.mkDerivation {
     substituteInPlace gkrellm-bfm.c \
       --replace-fail \
         'cb_interval_modified(GtkWidget *widget, GtkSpinButton *spin)' \
-        'cb_interval_modified(GtkSpinButton *spin, gpointer data)' \
+        'cb_interval_modified(GtkAdjustment *adjustment, GtkSpinButton *spin)' \
       --replace-fail \
         'update_interval = gtk_spin_button_get_value_as_int(spin);' \
-        '(void) data;
-        update_interval = gtk_spin_button_get_value_as_int(spin);'
+        '(void) adjustment;
+        update_interval = gtk_spin_button_get_value_as_int(spin);' \
+      --replace-fail \
+        'GtkWidget *tabs = NULL;' \
+        'GtkWidget *tabs = NULL;
+        GtkWidget *interval_spin = NULL;' \
+      --replace-fail \
+        'gkrellm_gtk_spin_button(main_box, NULL, update_interval,' \
+        'gkrellm_gtk_spin_button(main_box, &interval_spin, update_interval,' \
+      --replace-fail \
+        'cb_interval_modified, NULL, FALSE,' \
+        'NULL, NULL, FALSE,' \
+      --replace-fail \
+        '_("Updates per second"));' \
+        '_("Updates per second"));
+    g_signal_connect(G_OBJECT(gtk_spin_button_get_adjustment(
+        GTK_SPIN_BUTTON(interval_spin))), "value-changed",
+        G_CALLBACK(cb_interval_modified), interval_spin);'
 
     substituteInPlace Makefile \
       --replace-fail \
